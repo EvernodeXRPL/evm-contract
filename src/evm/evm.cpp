7fda2c914dd111376e9453e40545de3f9fb00806
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-11-16 13:06:04 +0530

    Execution infrastructure.

--- src/evm/evm.cpp
+++ src/evm/evm.cpp
@@ -1,28 +1,34 @@
 
 #include "../pchheader.hpp"
+#include "../util.hpp"
+#include "../sqlite.hpp"
 #include "evm_host.hpp"
 
 namespace evm
 {
-    int execute(sqlite3 &db, const std::string &code_buf, const std::string &input_buf)
+    const evmc::address bin2addr(std::string_view bin)
     {
-        const uint8_t *code = (uint8_t *)code_buf.data();
-        const size_t code_size = code_buf.size();
-        const uint8_t *input = (uint8_t *)input_buf.data();
-        const size_t input_size = input_buf.size();
+        evmc::address addr = {};
+        if (bin.size() == sizeof(addr.bytes))
+            memcpy(addr.bytes, bin.data(), sizeof(addr.bytes));
+        return addr;
+    }
 
+    int execute(sqlite3 *db, std::string_view addr, std::string_view input, std::string_view code, std::string &output)
+    {
+        evmc::address eaddr = bin2addr(addr);
         const evmc_uint256be value = {{0}};
-        const evmc_address addr = {{0, 1, 2}};
         const int64_t gas = 200000;
 
         struct evmc_message msg;
         memset(&msg, 0, sizeof(msg));
         msg.kind = EVMC_CALL;
-        msg.sender = addr;
-        msg.recipient = addr;
+        msg.sender = {};
+        msg.recipient = eaddr;
+        msg.code_address = eaddr;
         msg.value = value;
-        msg.input_data = input;
-        msg.input_size = input_size;
+        msg.input_data = input.size() ? (uint8_t *)input.data() : NULL;
+        msg.input_size = input.size();
         msg.gas = gas;
         msg.depth = 0;
         msg.flags = 0;
@@ -39,7 +45,8 @@ namespace evm
         const struct evmc_host_interface *host = &evmc::Host::get_interface();
         struct evmc_host_context *host_ctx = create_host_context(db, tx_context);
 
-        struct evmc_result result = evmc_execute(vm, host, host_ctx, EVMC_LONDON, &msg, code, code_size);
+        struct evmc_result result = evmc_execute(vm, host, host_ctx, EVMC_LONDON, &msg,
+                                                 (code.size() ? (uint8_t *)code.data() : NULL), code.size());
         printf("Execution result:\n");
         int exit_code = 0;
         if (result.status_code != EVMC_SUCCESS)
@@ -52,6 +59,7 @@ namespace evm
         }
         else
         {
+            // Success
             printf("  Gas used: %" PRId64 "\n", msg.gas - result.gas_left);
             printf("  Gas left: %" PRId64 "\n", result.gas_left);
             printf("  Output size: %zd\n", result.output_size);
@@ -60,16 +68,37 @@ namespace evm
             for (i = 0; i < result.output_size; i++)
                 printf("%02x", result.output_data[i]);
             printf("\n");
-            const evmc_bytes32 storage_key = {{0}};
-            evmc_bytes32 storage_value = host->get_storage(host_ctx, &msg.recipient, &storage_key);
-            printf("  Storage at 0x00..00: ");
-            for (i = 0; i < sizeof(storage_value.bytes) / sizeof(storage_value.bytes[0]); i++)
-                printf("%02x", storage_value.bytes[i]);
-            printf("\n");
+
+            output.resize(result.output_size);
+            memcpy(output.data(), result.output_data, result.output_size);
         }
         evmc_release_result(&result);
         destroy_host_context(host_ctx);
         evmc_destroy(vm);
         return exit_code;
+    }
+
+    int deploy(sqlite3 *db, std::string_view addr_hex, std::string_view code_hex)
+    {
+
+        std::string output;
+        std::string addr = util::hex2bin(addr_hex);
+        std::string code = util::hex2bin(code_hex);
+        evmc::uint256be balance = FULL_BALANCE;
+        if (sql::insert_account(db, addr, BINSTR(balance), code) != -1 &&
+            execute(db, addr, {}, code, output) == 0)
+            return sql::update_account_code(db, addr, output);
+
+        return -1;
+    }
+
+    int run(sqlite3 *db, std::string_view addr_hex, std::string_view input_hex, std::string &output)
+    {
+        std::string code;
+        std::string addr = util::hex2bin(addr_hex);
+        std::string input = util::hex2bin(input_hex);
+        if (sql::get_account_code(db, addr, code) == 1)
+            return execute(db, addr, input, code, output);
+        return -1;
     }
 }
--- src/evm/evm_host.hpp
+++ src/evm/evm_host.hpp
@@ -5,7 +5,7 @@
 
 namespace evm
 {
-    evmc_host_context *create_host_context(sqlite3 &db, evmc_tx_context tx_context);
+    evmc_host_context *create_host_context(sqlite3 *db, evmc_tx_context tx_context);
     void destroy_host_context(evmc_host_context *context);
 }
 
